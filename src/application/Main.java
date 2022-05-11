a07dc69cf97def7057e4b766e9ef374fa8ba6e79
Rafael Piva <rafa.piva12@gmail.com>
2022-05-11 15:38:10 -0300

    Calculadora de Circunferência
    Exercício 5  em JAVA calculadora de Circunferência

---
+++ src/application/Main.java
@@ -0,0 +1,35 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import util.Calculator;
+
+public class Main {
+	
+
+	
+	public static void main(String[]args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
+		System.out.println("Qual valor do Raio ?");
+		double raio = sc.nextDouble();
+		
+		double c = Calculator.circunferencia(raio);
+		double v = Calculator.volume(raio);
+		
+		System.out.printf("Circuferência: %.2f%n", c);
+		System.out.printf("Volume: %.2f%n", v);
+		System.out.printf("Valor de PI: %.2f%n",Calculator.PI);
+		
+		sc.close();
+		
+	}
+
+	
+	
+	
+	
+}
